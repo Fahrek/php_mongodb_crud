d98371ae2c5ab075e727d105611e984b8317d839
Fernando Gaitán <fernandoggaitan@gmail.com>
2014-09-01 15:43:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 
 try {
     $mongo = new Mongo();
-    $db = $mongo->selectDB('prueba');
-    $informe_documento = $db->selectCollection('informe');
-    $lista = $informe_documento->find();
+    $db = $mongo->selectDB('veterinaria');
+    $cliente_coleccion = $db->selectCollection('cliente');
 } catch (MongoConnectionException $e) {
     echo $e->getMessage();
     exit;
@@ -13,32 +12,62 @@ try {
     exit;
 }
 
+$lista = $cliente_coleccion->find();
+
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
+        <meta charset="utf-8">
         <title> Lista </title>
+        <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" /> 
     </head>
     <body>
-        <h1> Lista </h1>
-        <p>
-            <a href="guardar.php"> Crear nuevo </a>
-        </p>
-        <ul>
-            <?php while ($lista->hasNext()): ?>
-                <?php $item = $lista->getNext() ?>
-                <li> 
-                    <h2> <?php echo $item['titulo'] ?> </h2>
-                    <p> 
-                        <a href="guardar.php?ide=<?php echo $item['_id']  ?>"> Editar </a>
-                        |
-                        <a href="eliminar.php?ide=<?php echo $item['_id']  ?>"> Eliminar </a>
-                    </p>
-                    <hr />
-                </li>
-            <?php endwhile; ?>
-        </ul>
+        <div class="container">
+            <h1> Lista </h1>
+            <div class="row">
+                <div class="span4">
+                    <a href="guardar.php" class="btn btn-success"> Agregar nuevo cliente </a>
+                </div>
+            </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th> Nombre y apellido </th>
+                        <th> Dirección </th>
+                        <th> Teléfono </th>
+                        <th> Email </th>
+                        <th> Mascotas </th>
+                        <th>  </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($lista->hasNext()): ?>
+                        <?php $item = $lista->getNext() ?>
+                        <tr>
+                            <td> <?php echo $item['nombre_apellido'] ?> </td>
+                            <td> <?php echo $item['direccion'] ?> </td>
+                            <td> <?php echo $item['telefono'] ?> </td>
+                            <td> <?php echo $item['email'] ?> </td>
+                            <td> 
+                                <ul>
+                                    <?php foreach ($item['mascotas'] as $item2): ?> 
+                                        <li>
+                                            <?php echo $item2['nombre'] . ' - ' . $item2['detalle'] ?>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </td>
+                            <td>
+                                <a href="guardar.php?_id=<?php echo $item['_id'] ?>" class="btn btn-info"> Editar </a>
+                                <a href="eliminar.php?_id=<?php echo $item['_id'] ?>" class="btn btn-danger"> Eliminar </a>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
+        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
     </body>
 </html>
